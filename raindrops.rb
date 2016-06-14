9888aca0496f0c51987619f166a534f46255a8b0
emily.c.ritter <emily.c.ritter@gmail.com>
2016-06-13 21:20:16 -0500

    First successful run

--- raindrops.rb
+++ raindrops.rb
@@ -2,15 +2,15 @@ class Raindrops
   VERSION = 1
 
   def self.convert(num)
+    raindrop = ""
     [3, 5, 7].each do |prime|
-      raindrop = ""
       if (num%prime == 0)
         raindrop += 'Pling' if prime == 3
-        raindrop += 'Plang' if prime == 3
-        raindrop += 'Plong' if prime == 3
-      else; raindrop = num.to_s
+        raindrop += 'Plang' if prime == 5
+        raindrop += 'Plong' if prime == 7
       end
     end
+    raindrop = num.to_s if raindrop == ""
     return raindrop
   end
 
